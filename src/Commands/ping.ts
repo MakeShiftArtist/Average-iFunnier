a8b3904a1bbd5efd16b508936d656707bfbb43e7
MakeShiftArtist <lrmcbride4@gmail.com>
2022-05-18 00:11:18 -0500

    Added the Ping command

---
+++ src/Commands/ping.ts
@@ -0,0 +1,29 @@
+import Command from "../Objects/Command";
+
+export default new Command(
+	"ping",
+	"Pings the bot and receives response time",
+	async (interaction, client) => {
+		const embed = client.util
+			.embed()
+			.setTitle("Pong!")
+			.setColor("YELLOW")
+			.setTimestamp();
+
+		await interaction.reply({ embeds: [embed] });
+		if (interaction.inCachedGuild()) {
+			const msg = await interaction.fetchReply();
+			embed.setDescription(
+				`Bot Latency: \`${
+					msg.createdTimestamp - interaction.createdTimestamp
+				}ms\`\nWebSocket Latency: \`${interaction.client.ws.ping}ms\``
+			);
+			interaction.editReply({ embeds: [embed] });
+			return;
+		}
+		client.logger.warn("Ping failed due to channel not being cached", interaction);
+	},
+	{
+		devOnly: true,
+	}
+);
