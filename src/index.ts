74854ed392ad56b27cc297c307b327cee720bdf2
makeshiftartist <47653736+MakeShiftArtist@users.noreply.github.com>
2023-09-30 19:59:36 -0500

    feat(index): add dotenv and SIGTERM handling

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import AIClient from "./models/client/AIClient";
-import "dotenv/config";
+import dotenv from "dotenv";
+
+// This allows you to easily configure multiple environment variables.
+dotenv.config({
+	path: `.env.${process.env.NODE_ENV}`,
+});
 
 const client = new AIClient({
 	discord_token: process.env.DISCORD_TOKEN!,
 	ifunny_basic: process.env.IFUNNY_BASIC!,
 	ifunny_bearer: process.env.IFUNNY_BEARER!,
+});
+
+// This event is triggered by docker stop, and allows the bot to gracefully shutdown.
+process.on("SIGTERM", () => {
+	client.discord.destroy();
+	process.exit(0);
 });
 
 client.on("ready", () => {
