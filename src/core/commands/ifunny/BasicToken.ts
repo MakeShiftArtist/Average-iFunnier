084de98eae645ad336f4bf78b9b279b5cd1b615e
makeshiftartist <47653736+MakeShiftArtist@users.noreply.github.com>
2023-09-30 20:21:31 -0500

    feat(basics): add support for custom parameters

--- src/core/commands/ifunny/BasicToken.ts
+++ src/core/commands/ifunny/BasicToken.ts
@@ -1,4 +1,5 @@
 import Command from "../../../models/discord/Command";
+import ifunny from "ifunny.ts";
 
 export default new Command(
 	{
@@ -7,15 +8,56 @@ export default new Command(
 		category: "ifunny",
 	},
 	async (bot, interaction) => {
+		let length = interaction.options.getNumber("length", false) as
+			| 112
+			| 156
+			| null;
+		let clientId = interaction.options.getString("client_id", false);
+		let clientSecret = interaction.options.getString("client_secret", false);
+
 		await interaction.reply({
 			ephemeral: true,
 			embeds: [
 				new bot.embeds.default(bot, {
 					title: "Basic Token",
-					description: `\`${bot.ifunny.util.createBasicAuth()}\``,
+					description: `\`${bot.ifunny.util.createBasicAuth({
+						length: length ? length : 112,
+						clientId: clientId ? clientId : ifunny.DEFAULT_CONFIG.clientId,
+						clientSecret: clientSecret
+							? clientSecret
+							: ifunny.DEFAULT_CONFIG.clientSecret,
+					})}\``,
 					timestamp: new Date(),
 				}),
 			],
 		});
 	}
-);
+)
+	.addNumberOption((option) => {
+		return option
+			.setName("length")
+			.setDescription("The length of the token to generate")
+			.setRequired(true)
+			.addChoices(
+				{
+					name: "112",
+					value: 112,
+				},
+				{
+					name: "156",
+					value: 156,
+				}
+			);
+	})
+	.addStringOption((option) => {
+		return option
+			.setName("client_id")
+			.setDescription("The client id to generate the token with")
+			.setRequired(false);
+	})
+	.addStringOption((option) => {
+		return option
+			.setName("client_secret")
+			.setDescription("The client secret to generate the token with")
+			.setRequired(false);
+	});
